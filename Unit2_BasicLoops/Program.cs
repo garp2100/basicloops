2e8326d93b907938d5776ff133a76e712fed91a2
Gustavo Rivera <garp2100@gmail.com>
2022-06-29 16:24:57 -0400

    Fixing typo in "KeyPad"

--- Unit2_BasicLoops/Program.cs
+++ Unit2_BasicLoops/Program.cs
@@ -11,26 +11,29 @@
 //}
 
 //Prompt the user for a number. Use a for loop to output all the numbers from that number to 0. After that loop finishes, use another loop to output all the numbers from 0 to that number.
-//Console.WriteLine("Please enter a number: ");
-//int input = int.Parse(Console.ReadLine());
-//for (int i = 0; i < input; i++)
-//    {
-//        Console.WriteLine(i);
-//    }
-//    for (int j = input; j > 0; j--)
-//    {
-//        Console.Write(j);
-//    }
-//while (true)
-//{
-//    Console.WriteLine("Would you like to restart? y/n.");
-//    string restartValue = Console.ReadLine().ToLower();
-//    if (restartValue == "y")
-//        continue;
-//    else
-//        Console.WriteLine("Goodbye!");
-//    break;
-//}
+while (true)
+{
+    Console.WriteLine("Please enter a number: ");
+    int input = int.Parse(Console.ReadLine());
+    for (int i = 0; i < input; i++)
+    {
+        Console.Write(i);
+    }
 
+    for (int j = input; j > 0; j--)
+    {
+        Console.Write(j);
+    }
 
+    while (true)
+    {
+        Console.WriteLine("Would you like to restart? y/n.");
+        string restartValue = Console.ReadLine().ToLower();
+        if (restartValue == "y")
+            break;
+        else
+            Console.WriteLine("Goodbye!");
+        Environment.Exit(0);
+    }
+}
 
